ed6254f792e146ff5b6e8ed95ce0089f6bc24fb5
Korney-EV <korneychik.e.v@gmail.com>
2021-12-19 20:10:48 +0300

    Использование аннотации @PreAuthorization

--- src/main/java/by/rekhaus/springsecurity/rest/DeveloperRestControllerV1.java
+++ src/main/java/by/rekhaus/springsecurity/rest/DeveloperRestControllerV1.java
@@ -3,6 +3,7 @@ package by.rekhaus.springsecurity.rest;
 import by.rekhaus.springsecurity.model.Developer;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,18 +25,21 @@ public class DeveloperRestControllerV1 {
     }
 
     @GetMapping ("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
     public Developer getById (@PathVariable Long id) {
         return DEELOPERS.stream().filter(developer -> developer.getId().equals(id))
                 .findFirst()
                 .orElse(null);
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('developers:write')")
     public Developer create(@RequestBody Developer developer) {
         this.DEELOPERS.add(developer);
         return developer;
     }
 
     @DeleteMapping ("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
     public void deleteById (@PathVariable Long id) {
         this.DEELOPERS.removeIf(developer -> developer.getId().equals(id));
     }
